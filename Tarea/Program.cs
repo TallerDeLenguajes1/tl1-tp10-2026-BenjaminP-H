40ae25ad50844015c4b0e7905871fd742007e4a4
Benjamin <benjaminpereina@gmail.com>
2026-05-08 20:17:30 -0300

    Presento por consola la informacion del Json

---
+++ Tarea/Program.cs
@@ -0,0 +1,23 @@
+﻿using Tarea;
+using System;
+using System.Text.Json;
+using System.ComponentModel;
+
+
+HttpClient client =  new HttpClient();
+
+var url = "https://jsonplaceholder.typicode.com/todos/";
+
+HttpResponseMessage response = await client.GetAsync(url);
+
+response.EnsureSuccessStatusCode();
+
+
+string responseBody = await response.Content.ReadAsStringAsync();
+List<Tareas> listTareas = JsonSerializer.Deserialize<List<Tareas>>(responseBody);
+
+
+foreach(var tareas in listTareas)
+{
+    Console.WriteLine("ID: " + tareas.id + " Nombre: " + tareas.title + " Estado: " + tareas.completed);
+}
---
+++ Tarea/Tarea.cs
@@ -0,0 +1,9 @@
+namespace Tarea;
+
+public class Tareas
+{
+    public int userId {get; set;}
+    public int id { get; set;}
+    public string title {get; set;}
+    public bool completed {get; set;}
+}
