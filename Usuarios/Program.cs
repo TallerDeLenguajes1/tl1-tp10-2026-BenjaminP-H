03b590c72d997efb1795f98638ff8d3cf3c1f9db
Benjamin Pereira Herrera <benjaminpereina@gmail.com>
2026-06-17 18:34:40 -0300

    Segundo Punto Terminado

---
+++ Usuarios/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+using Usuarios;
+
+HttpClient client = new HttpClient(); 
+var url = "https://jsonplaceholder.typicode.com/users/"; 
+HttpResponseMessage response = await client.GetAsync(url);
+response.EnsureSuccessStatusCode(); 
+string responseBody = await response.Content.ReadAsStringAsync();
+List<Usuario> listaUsuarios = JsonSerializer.Deserialize<List<Usuario>>(responseBody);
+
+Console.WriteLine("====================================================");
+Console.WriteLine("=========== Lista De Usuarios Traducidas ===========");
+Console.WriteLine("====================================================");
+
+for(int i = 0 ; i < 5 ; i++)
+{
+    Console.WriteLine($"=========== Usuario {i + 1} ===========");
+    Console.WriteLine($"Nombre: {listaUsuarios[i].name}");
+    Console.WriteLine($"Correo: {listaUsuarios[i].email}");
+    Console.WriteLine($"Domicilio: {listaUsuarios[i].address.city}");
+}
+
