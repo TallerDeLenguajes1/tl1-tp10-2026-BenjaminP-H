902a18f650ac4c0fa806b85cfb286ed92c60a661
Benjamin Pereira Herrera <benjaminpereina@gmail.com>
2026-06-17 03:44:32 -0300

    Primer punto Terminado

--- Tarea/Program.cs
+++ Tarea/Program.cs
@@ -1,7 +1,5 @@
 ﻿using Tarea;
-using System;
 using System.Text.Json;
-using System.ComponentModel;
 
 
 HttpClient client =  new HttpClient();
@@ -14,10 +12,52 @@ response.EnsureSuccessStatusCode();
 
 
 string responseBody = await response.Content.ReadAsStringAsync();
-List<Tareas> listTareas = JsonSerializer.Deserialize<List<Tareas>>(responseBody);
 
+List<Tareas> listaTarea = JsonSerializer.Deserialize<List<Tareas>>(responseBody);
 
-foreach(var tareas in listTareas)
+List<Tareas> tareasPendientes = new List<Tareas>();
+List<Tareas> tareasCompletadas = new List<Tareas>();
+
+Console.WriteLine("=========== Lista De Tareas Traducidas ===========;");
+Console.WriteLine();
+foreach(var tareas in listaTarea)
+{
+    Console.WriteLine("ID: " + tareas.id + " Nombre: " + tareas.title + " Estado: " + tareas.completed);
+    if(tareas.completed)
+    {
+        tareasCompletadas.Add(tareas);
+    }
+    else
+    {
+        tareasPendientes.Add(tareas);
+    }
+}
+Console.WriteLine();
+Console.WriteLine("=========== Tareas Completadas ===========");
+Console.WriteLine();
+foreach(var tareas in tareasCompletadas)
 {
     Console.WriteLine("ID: " + tareas.id + " Nombre: " + tareas.title + " Estado: " + tareas.completed);
 }
+Console.WriteLine();
+Console.WriteLine("=========== Tareas Pendientes ===========");
+Console.WriteLine();
+foreach(var tareas in tareasPendientes)
+{
+    Console.WriteLine("ID: " + tareas.id + " Nombre: " + tareas.title + " Estado: " + tareas.completed);
+}
+
+Console.WriteLine();
+Console.WriteLine("===========  Lista de Tareas Formato Json ===========");
+Console.WriteLine();
+
+string json = JsonSerializer.Serialize(
+    listaTarea,
+    new JsonSerializerOptions
+    {
+     WriteIndented = true   
+    });
+
+File.WriteAllText("tareas.json", json);
+
+Console.WriteLine("Archivo generado correctamente.");
